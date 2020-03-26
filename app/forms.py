378378e9adb2e6741504e565301dfb1fb9c8e06d
May Xe <pristi.stanice.hanoi@gmail.com>
2020-03-26 10:57:31 +0900

    apply card design for list screen

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,7 @@ class ItemForm(forms.ModelForm):
         # fieldsに追記すると項目として追加される
         fields = ('name','staff_1','price_1','staff_2','price_2','staff_3','price_3','staff_4','price_4','staff_5','price_5','memo')
         widgets = {
-                    'name': forms.TextInput(attrs={'placeholder':'Ví dụ về mục：Nguyen thi Thuy'}),
+                    'name': forms.TextInput(attrs={'placeholder':'Ví dụ về mục：Nguyen thi Thuy','add_class':'form-control'}),
                     #'price_total': forms.TextInput(attrs={'disabled':'disabled'}), #read-only
                     'staff_1': forms.TextInput(attrs={'placeholder':'Ví dụ về mục：Trang'}),
                     'price_1': forms.TextInput(attrs={'placeholder':'Ví dụ về mục：60'}),
